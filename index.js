b8c50a7ec672ef3acc02b15d1bb6bdcd53623b52
Arko <arkoroyb@gmail.com>
2022-11-24 13:41:37 +0600

    basic setup and one api link created

--- index.js
+++ index.js
@@ -1,14 +1,53 @@
 const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000
 
 
 const app = express()
-const port = process.env.PORT || 5000
+app.use(cors())
+app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.et115mk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+
+
+async function run() {
+    try {
+        const fridgeCollection = client.db('puranaBazar').collection('AllFridge')
+
+        app.get('/category/:id', async (req,res) => {
+            const ID = req.params.id;
+            if(ID === 4) {
+                const query = {}
+            } else {
+                const query = {_id: ID}
+            }
+            const query = {}
+            
+            const allFridge = await fridgeCollection.find(query).toArray();
+            res.send(allFridge);
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.log())
 
 
 app.get('/', (req,res)=> {
     res.send("purana-bazar server is running")
 })
-
 
 app.listen(port, ()=> {
     console.log(`server is running on ${port}`);
